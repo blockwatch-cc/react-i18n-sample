914611f0d0ed18895e0843bef7b8fd4f529df057
Alexander Eichhorn <alex@kidtsunami.com>
2020-09-12 15:45:44 +0200

    Updates

---
+++ precache-manifest.797c73de35849ea9b92123ecea4c6805.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e1c964febd92c3e65237ea6d8ee9d6cf",
+    "url": "/react-i18n-sample/index.html"
+  },
+  {
+    "revision": "6583b5749bd0ef030f95",
+    "url": "/react-i18n-sample/static/css/main.4882d7f5.chunk.css"
+  },
+  {
+    "revision": "6515784b4f51d775e873",
+    "url": "/react-i18n-sample/static/js/2.2f43438b.chunk.js"
+  },
+  {
+    "revision": "db6214b36b3ad0ee76c6a58d9aac59de",
+    "url": "/react-i18n-sample/static/js/2.2f43438b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6583b5749bd0ef030f95",
+    "url": "/react-i18n-sample/static/js/main.e44ea2fc.chunk.js"
+  },
+  {
+    "revision": "33a9ab3f376bcdacfe4f",
+    "url": "/react-i18n-sample/static/js/runtime-main.b3fcc3d2.js"
+  }
+]);
